7665161b3b2a9ff0b965993da8bbd586edc2c3ed
Babajide777 <j.oyafemi@gmail.com>
2021-05-09 05:13:49 -0500

    homepage almost done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
-import "./App.css";
+import React from "react";
+import { Route, Switch as Switching } from "react-router";
+import Sidebar from "./components/Sidebar";
+import styled from "styled-components";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <p>Portfolio ....</p>
-    </div>
+    <>
+      <Sidebar />
+      <MainContentStyled>
+        <div className="lines">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+          <div className="line-3"></div>
+          <div className="line-4"></div>
+        </div>
+
+        <Switching>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          {/* <Route path="/about" exact>
+            <AboutPage />
+          </Route>
+          <Route path="/resume" exact>
+            <ResumePage />
+          </Route>
+          <Route path="/portfolios" exact>
+            <PortfoliosPage />
+          </Route>
+          <Route path="/blogs" exact>
+            <BlogsPage />
+          </Route>
+          <Route path="/contact" exact>
+            <ContactPage />
+          </Route> */}
+        </Switching>
+      </MainContentStyled>
+    </>
   );
 }
+
+const MainContentStyled = styled.main`
+  position: relative;
+  margin-left: 16.3rem;
+  min-height: 100vh;
+  @media screen and (max-width: 1200px) {
+    margin-left: 0;
+  }
+  .lines {
+    position: absolute;
+    min-height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    opacity: 0.4;
+    z-index: -1;
+    .line-1,
+    .line-2,
+    .line-3,
+    .line-4 {
+      width: 1px;
+      min-height: 100vh;
+      background-color: var(--border-color);
+    }
+  }
+`;
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+// import { Container, Nav, Navbar } from "react-bootstrap";
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+
+const Header = () => {
+  return (
+    <>
+      {/* <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" id="caps">
+        <Container>
+          <Navbar.Brand href="#home">J.OYAFEMI</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto pointer">
+              <Nav.Link href="#deets">Home</Nav.Link>
+              <Nav.Link href="#deets">About Me</Nav.Link>
+              <Nav.Link href="#deets">Services</Nav.Link>
+              <Nav.Link href="#deets">Portofolio</Nav.Link>
+              <Nav.Link href="#deets">Contact</Nav.Link>
+              <Nav.Link></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar> */}
+
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+//<FontAwesomeIcon icon={faFacebook} />
+export default Header;
